2f06491daa624eddba408bf01b1d039f8e23f681
fhmyngrh <85100216+fhmyngrh@users.noreply.github.com>
2021-08-15 01:15:11 +0700

    Api Info Bot

---
+++ translation.py
@@ -0,0 +1,16 @@
+class Translation(object):
+    START_TEXT = """Hai!
+Kegunaan Bot ini untuk membantu anda mengambil API ID & API HASH Telegram Dengan mudah.
+Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format +62(No HP).
+"""
+    AFTER_RECVD_CODE_TEXT = """No HP Diterima!
+Sekarang silakan kirim kode Telegram yang Anda terima dari Telegram!
+
+Kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
+jika Anda tidak mempercayai dev bot ini, Ngambil Manual aja Goblokk 😁🙏
+"""
+    BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
+    ERRED_PAGE = "Error!!! Gagal Mendapatkan id aplikasi. \n\n Kontak saya : @UnrealNakaa"
+    CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang"
+    IN_VALID_CODE_PVDED = "Masukan Kodelogin Telegram"
+    IN_VALID_PHNO_PVDED = "Masukan Nomor Dengan Format +(Kode Negara). Contoh +62(No HP)"
